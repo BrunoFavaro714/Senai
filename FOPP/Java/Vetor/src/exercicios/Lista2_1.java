087fa02f9a01d9715ff4c50af56a5f049d59d994
BrunoFavaro714 <brunofavaro.714@gmail.com>
2022-02-18 11:22:19 -0300

    02/18/2022

---
+++ FOPP/Java/Vetor/src/exercicios/Lista2_1.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class Lista2_1 {
+
+	public static void main(String[] args) {
+		mostrar();
+	}
+	public static void mostrar() {
+		
+		int vetor[] = {2, 4, 5, 6, 8};
+		
+		System.out.println("Indice:\t\tValor:");
+		for(int i = 0; i < vetor.length; i++) {
+		System.out.println("Vetor " + (i + 1) +":\t"+vetor[i]);
+		}
+	}
+}
---
+++ FOPP/Java/Vetor/src/exercicios/Lista2_2.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Lista2_2 {
+
+	
+	public static Scanner input;
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	public static void leVetor() {
+	
+		
+	}
+	public static void mostra() {
+		
+	}
+	public static void maioridade() {
+		
+	}
+}
