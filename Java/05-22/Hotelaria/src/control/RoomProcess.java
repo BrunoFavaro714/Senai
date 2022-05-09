1ce14c066b9dd8adbed3974c35c5a31a98f0b85c
BrunoFavaro714 <brunofavaro.714@gmail.com>
2022-05-09 10:40:04 -0300

    05/09/2022

--- Java/05-22/Hotelaria/src/control/RoomProcess.java
+++ Java/05-22/Hotelaria/src/control/RoomProcess.java
@@ -1,5 +1,20 @@
 package control;
 
+import java.util.ArrayList;
+
+import model.Room;
+
 public class RoomProcess {
 
+	public static ArrayList<Room> rooms = new ArrayList<>();
+	private static RoomDAO rd = new RoomDAO();
+	
+	public static void abrir() {
+		rooms = rd.ler();
+	}
+	
+	public static void salvar() {
+		rd.escrever(rooms);
+	}
+	
 }
--- Java/05-22/Hotelaria/src/model/Room.java
+++ Java/05-22/Hotelaria/src/model/Room.java
@@ -3,6 +3,7 @@ package model;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Currency;
 import java.util.Date;
 import java.util.Locale;
 
@@ -15,7 +16,6 @@ public class Room {
 	private Date entrada;
 	private Date saida;
 	private float perNoite;
-	private float servicoQuarto;
 	private float valorTotal;
 	
 	private final Locale BRASIL = new Locale("pt", "BR");
@@ -23,7 +23,7 @@ public class Room {
 	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	
-	public Room(int quarto, String nomeCompleto, String telefone, String entrada, String saida, float perNoite, float servicoQuarto, float valorTotal) {
+	public Room(int quarto, String nomeCompleto, String telefone, String entrada, String saida, float perNoite) {
 		
 		this.quarto = quarto;
 		this.nomeCompleto = nomeCompleto;
@@ -35,9 +35,38 @@ public class Room {
 			System.out.println(e);
 		}
 		this.perNoite = perNoite;
-		this.servicoQuarto = servicoQuarto;
-		this.valorTotal = valorTotal;
 	}
 	
-	
+
+	public void ValorTotal() {
+		long estadia =  saida.getTime() - entrada.getTime();
+		this.valorTotal = perNoite * estadia;
+	}
+
+	public Room(String linha) {
+		df.setCurrency(Currency.getInstance(BRASIL));
+		
+		this.quarto = Integer.parseInt(linha.split(";")[0]);
+		this.nomeCompleto = linha.split(";")[1];
+		this.telefone = linha.split(";")[2];
+		try {
+			this.entrada = sdf.parse(linha.split(";")[3]);
+			this.saida = sdf.parse(linha.split(";")[4]);
+		}catch(ParseException e) {
+			System.out.println(e);
+		}
+		this.perNoite = Float.parseFloat(linha.split(";")[5]);
+		this.valorTotal = Float.parseFloat(linha.split(";")[6]);
+	}
+
+	@Override
+	public String toString() {
+		return quarto + " " + nomeCompleto + " " + telefone + " " 
+				+ entrada + " " + saida + " " + perNoite + " " + valorTotal;
+	}
+
+	public String toCSV() {
+		return quarto + ";" + nomeCompleto + ";" + telefone + ";" 
+				+ entrada + ";" + saida + ";" + perNoite + ";" + valorTotal;
+	}
 }
