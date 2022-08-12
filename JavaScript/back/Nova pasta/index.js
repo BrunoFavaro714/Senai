1b1a8b8d8d2d0f052b3e58c120c2936b3c5226af
BrunoFavaro714 <brunofavaro.714@gmail.com>
2022-08-12 11:07:24 -0300

    12-08-2022

---
+++ JavaScript/back/Nova pasta/index.js
@@ -0,0 +1,11 @@
+function a(){
+    var x1
+    var x2
+    var x3
+    var y = 11
+    var z = 12
+    x1 = z++ == ++y
+    x2 = 10 + "casa"
+    x3 = y+1 === z && 10 != "20"
+    console.log(++y)
+}
--- JavaScript/back/aula2/pedidos/index.js
+++ JavaScript/back/aula2/pedidos/index.js
@@ -10,11 +10,13 @@ const con = mysql.createConnection({
 
 app.use(express.json());
 
-app.get('/pedidos', (req, res) =>{
+app.post('/pedidos', (req, res) =>{
     let nome = req.query.priNome
     let sobrenome = req.query.sobrenome
     let endereco = req.query.endereco
     let telefones = req.query.telefones
+
+    
 
     let cliente = `insert into clientes value(null,'${nome}','${sobrenome}','${endereco}')`
     // let clienteTel = `insert into telefones value(${},'${telefones}')`
@@ -22,18 +24,11 @@ app.get('/pedidos', (req, res) =>{
     con.query(cliente, (err, result) => {
         if(err == null) {
             res.json("Dados recebidos com sucesso e enviados para nosso Banco de Dados");
-            console.log(result.insertId);
+            var idCliente = result.insertId;
         }else{
             res.json("Dados recebidos com sucesso, porém não enviar para nosso Banco de Dados");
         }
     });
-    // con.query(clienteTel, (err, result) => {
-    //     if(err == null) {
-    //         res.json("Dados recebidos com sucesso e enviados para nosso Banco de Dados");
-    //     }else{
-    //         res.json("Dados recebidos com sucesso, porém não enviar para nosso Banco de Dados");
-    //     }
-    // });
 });
 
 app.listen(3000, () => {
