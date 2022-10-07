763dc0af45515d6b5d3f0af6cc09026e37b269f0
BrunoFavaro714 <brunofavaro.714@gmail.com>
2022-10-07 11:26:39 -0300

    .

---
+++ 2ºSemestre/JavaScript/back/avaliação/back/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const solicitacoes = require('./src/routes/solicitacoesRoutes.js');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(solicitacoes);
+
+
+app.listen(3000, () => {
+    console.log('listening')
+})
