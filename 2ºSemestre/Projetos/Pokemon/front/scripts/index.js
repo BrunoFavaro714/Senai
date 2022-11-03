982be41ceedbd7013148069aaccc5a203e002148
BrunoFavaro714 <brunofavaro.714@gmail.com>
2022-11-03 11:28:34 -0300

    .

--- 2ºSemestre/Projetos/Pokemon/front/scripts/index.js
+++ 2ºSemestre/Projetos/Pokemon/front/scripts/index.js
@@ -4,12 +4,14 @@ const load = () => {
 }
 
 const PokemonEscolhido = () => {
-    let chosenPoke = Math.round(Math.random() * 152);
+    let chosenPoke = Math.round(Math.random() * 151)+1;
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${chosenPoke}`)
     .then(resp => { return resp.json() })
     .then(pokemon => {
         document.querySelector('#suaSprite').src = pokemon.sprites.versions["generation-v"]["black-white"].animated.back_default;
+
+        choseMoves(pokemon.moves)
 
         let hp = (2 * pokemon.stats[0].base_stat)/100;
 
@@ -31,4 +33,14 @@ const PokemonInimigo = () => {
         document.querySelector('#otherHealth').max = hp+11;
         document.querySelector('#otherHealth').value = hp+11;
     })
+}
+
+const choseMoves = (moves) => {
+    let rngMove =[];
+
+    for(let i = 0; i < 4; i++){
+        rngMove[i] = Math.round(Math.random() * moves.length);
+    }
+
+    console.log( rngMove)
 }
