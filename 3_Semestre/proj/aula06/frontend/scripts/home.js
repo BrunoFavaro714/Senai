3273e0e71041328fafe109c85f7ede14ce6067d3
BrunoFavaro714 <brunofavaro.714@gmail.com>
2023-03-13 11:30:15 -0300

    AgroTech

--- 3_Semestre/proj/aula06/frontend/scripts/home.js
+++ 3_Semestre/proj/aula06/frontend/scripts/home.js
@@ -10,12 +10,17 @@ const load = () => {
 }
 const mudarSetor = (setor) => {
     let graphis = document.querySelector('.grafico-analise').querySelectorAll('div');
+    let side = document.querySelector('.sidebar').querySelectorAll('div')
 
     graphis.forEach(g => {
         g.classList.add('model')
     })
+    side.forEach(s => {
+        s.classList.add('away');
+    })
 
-    document.querySelector(`.graph-container-${setor}`).classList.remove('model')
+    document.querySelector(`.graph-container-${setor}`).classList.remove('model');
+    document.querySelector(`.${setor}`).classList.remove('away');
 }
 
 const preencher = () => {
@@ -31,8 +36,8 @@ const preencher = () => {
 
             newDisponibilidade.querySelector('.placa').innerHTML = info.placa;
             newDisponibilidade.querySelector('.modelo').innerHTML = `${modelo[0]} ${modelo[1]}, ${modelo[2]}`;
-            newDisponibilidade.querySelector('.funcao').innerHTML = info.funcao;
-            newDisponibilidade.querySelector('.setor').innerHTML = info.setor;
+            newDisponibilidade.querySelector('.funcao').innerHTML += info.funcao;
+            newDisponibilidade.querySelector('.setor').innerHTML += info.setor;
             newDisponibilidade.querySelector('.dispon').innerHTML = info.disponibilidade == true ? 'Disponivel' : 'Ocupado';
             if(!info.disponibilidade){
                 newDisponibilidade.querySelector('.iconDisponivel').classList.add('covered');
@@ -41,11 +46,13 @@ const preencher = () => {
             
             newDisponibilidade.querySelector('.expand').addEventListener('click', () => {
                 newDisponibilidade.querySelector('.infos-geral').classList.toggle('model');
+                newDisponibilidade.querySelector('.iconExpand').classList.toggle('model');
+                newDisponibilidade.querySelector('.iconCollaps').classList.toggle('model');
             })
 
             newDisponibilidade.querySelector('.preventiva').innerHTML += info.last_prev.slice(0,10);
             newDisponibilidade.querySelector('.motorista').innerHTML += info.motorista;
-            newDisponibilidade.querySelector('.detalhes').innerHTML += `${modelo[3]}`;
+            newDisponibilidade.querySelector('.detalhes').innerHTML = `${modelo[3]}`;
 
             disponibilidade.appendChild(newDisponibilidade);
 
@@ -57,15 +64,19 @@ const preencher = () => {
                     newManutencao.classList.remove('model');
 
                     newManutencao.querySelector('.placa').innerHTML = info.placa;
-                    newManutencao.querySelector('.dataIni').innerHTML = manus.data_inicio.slice(0,10);
-                    newManutencao.querySelector('.previsao').innerHTML = manus.tempo_estimado;
+                    newManutencao.querySelector('.dataIni').innerHTML += manus.data_inicio.slice(0,10);
+                    newManutencao.querySelector('.previsao').innerHTML += manus.tempo_estimado;
 
-                    newManutencao.querySelector('.modelo').innerHTML = `${modelo[0]} ${modelo[1]}, ${modelo[2]}, com ${modelo[3]}`;
-                    newManutencao.querySelector('.funcao').innerHTML = info.funcao;
-                    newManutencao.querySelector('.setor').innerHTML = info.setor;
-                    newManutencao.querySelector('.motorista').innerHTML = info.motorista;
-                    newManutencao.querySelector('.valor').innerHTML = `R$ ${manus.valor}`;
-                    newManutencao.querySelector('.descricao').innerHTML = manus.descricao;
+                    newManutencao.querySelector('.expandManu').addEventListener('click', () => {
+                        newManutencao.querySelector('.manutencao-geral').classList.toggle('model')
+                    })
+
+                    newManutencao.querySelector('.modelo').innerHTML = `${modelo[0]} ${modelo[1]}, ${modelo[2]}`;
+                    newManutencao.querySelector('.funcao').innerHTML += info.funcao;
+                    newManutencao.querySelector('.setor').innerHTML += info.setor;
+                    newManutencao.querySelector('.motorista').innerHTML += info.motorista;
+                    newManutencao.querySelector('.valor').innerHTML += `R$ ${manus.valor}`;
+                    newManutencao.querySelector('.descricao').innerHTML += manus.descricao;
 
                     manutencao.appendChild(newManutencao);
                 }
