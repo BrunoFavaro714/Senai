0285d1f878a3bcdd05b0ec38a1243498482be929
BrunoFavaro714 <brunofavaro.714@gmail.com>
2023-04-06 11:01:58 -0300

    .

---
+++ 3_Semestre/TCC/agenda/backend/src/model/afazeres.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const afazeresSchema = new Schema({
+    id_usuario: {type: String, required: true},
+    data: {type: Date, required: true},
+    conteudo: {type: String, required: true},
+    cor: {type: String, required: true},
+    urgencia: {type: String, required: true},
+    status: {
+        concluido: {type: Boolean, required: true},
+        atrazado: {type: Boolean, required: true},
+    }
+})
+
+module.exports = mongoose.model('afazeres', afazeresSchema)
