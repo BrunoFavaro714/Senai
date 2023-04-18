b83dd7c4e4b1448c5ddc75ac11001983cc4c2285
BrunoFavaro714 <brunofavaro.714@gmail.com>
2023-04-18 11:09:32 -0300

    .

--- 3_Semestre/TCC/agenda/frontend/scripts/home.js
+++ 3_Semestre/TCC/agenda/frontend/scripts/home.js
@@ -1,6 +1,6 @@
 const tagDias = document.querySelector('.dias'),
-currentDate = document.querySelector('.mes'),
-prevNextIcon = document.querySelectorAll('.icon span');
+currentDate = document.querySelector('.current-date'),
+prevNextIcon = document.querySelectorAll('.icons span');
 
 let date = new Date(),
 currYear = date.getFullYear(),
@@ -16,17 +16,71 @@ const carregarCalendario = () => {
 
     let liTag = "";
     for(let i = primeiroDiaMes; i > 0; i--){
-        liTag += `<li class="inativo">${ultimaDataMesPassado - i + 1}</li>`;
+        liTag += `<li class="inativo" onclick="clicarDiaInativo(${currMonth - 1}, ${ultimaDataMesPassado - i + 1})">${ultimaDataMesPassado - i + 1}</li>`;
     }
     for(let i = 1; i <= ultimaDataMes; i++){
         let hoje = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "ativo" : "";
 
-        liTag += `<li class="${hoje}">${i}</li>`
+        liTag += `<li class="${hoje}" onclick="clicarDia(${i})">${i}</li>`
     }
     for(let i = ultimoDiaMes; i < 6; i++){
-        liTag += `<li class="inativo">${i - ultimoDiaMes + 1}</li>`
+        liTag += `<li class="inativo" onclick="clicarDiaInativo(${currMonth + 1}, ${i - ultimoDiaMes + 1})">${i - ultimoDiaMes + 1}</li>`
     }
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
     tagDias.innerHTML = liTag;
 }
+
 carregarCalendario();
+
+prevNextIcon.forEach(icon => { 
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        if(currMonth < 0 || currMonth > 11) {
+            date = new Date(currYear, currMonth, new Date().getDate());
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        } else {
+            date = new Date();
+        }
+        carregarCalendario();
+    });
+});
+
+const clicarDia = (dia) => {
+    let dias = document.querySelector('.dias').querySelectorAll('li');
+    dias.forEach(d => {
+        if(d.classList != "inativo"){
+            if(d.innerHTML == dia){
+                document.querySelector('.ativo').classList.remove("ativo");
+                d.classList.add("ativo")
+                console.log(d)
+            }
+        }
+    })
+}
+
+const clicarDiaInativo = (mes, dia) => {
+    currMonth = mes
+    if(currMonth < 0 || currMonth > 11) {
+        date = new Date(currYear, currMonth, new Date().getDate());
+        currYear = date.getFullYear();
+        currMonth = date.getMonth();
+    } else {
+        date = new Date();
+    }
+    carregarCalendario();
+
+    if(currMonth == new Date().getMonth()){
+        clicarDia(dia)
+    }else{
+        let dias = document.querySelector('.dias').querySelectorAll('li');
+    dias.forEach(d => {
+        if(d.classList != "inativo"){
+            if(d.innerHTML == dia){
+                d.classList.add("ativo")
+                console.log(d)
+            }
+        }
+    })
+    }
+}
